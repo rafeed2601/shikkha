66ee6ce80400ed36c98122f87545f7dbf26914d7
Rafeed Bin Akbar <67189529+rafeed2601@users.noreply.github.com>
2022-08-16 21:13:04 +0600

    login page creation done

---
+++ resources/views/login.blade.php
@@ -0,0 +1,21 @@
+@extends('master')
+@section("content")
+<div class="container custom-login">
+    <div class="row">
+        <div class="col-sm-4 col-sm-offset-4">
+            <form action="login" method="POST">
+                <div class="form-group">
+                    @csrf
+                <label for="myEmail">Email address</label>
+                <input type="email" name="email" class="form-control" id="myEmail" placeholder="Email">
+                </div>
+                <div class="form-group">
+                <label for="myPassword">Password</label>
+                <input type="password" name="password" class="form-control" id="myPassword" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
